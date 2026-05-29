b1b62bd23910f8ed5bdd4fdd39967beb69abf5cd
unknown <yzhan718@its.jnj.com>
2026-05-29 14:25:48 +0800

    chore: update app branding

--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -3,8 +3,9 @@
     <div class="site-content__wrapper">
       <div class="site-content">
         <div class="brand-info">
-          <h2 class="brand-info__text">renren-fast-vue</h2>
-          <p class="brand-info__intro">renren-fast-vue基于vue、element-ui构建开发，实现renren-fast后台管理前端功能，提供一套更优的前端解决方案。</p>
+          <img class="brand-info__logo" src="~@/assets/img/xianmu-logo.png" alt="鲜牧ERP系统">
+          <h2 class="brand-info__text">鲜牧ERP系统</h2>
+          <p class="brand-info__intro">鲜牧供应链进销存与销售协同管理平台。</p>
         </div>
         <div class="login-main">
           <h3 class="login-title">管理员登录</h3>
@@ -144,11 +145,16 @@
       margin: 220px 100px 0 90px;
       color: #fff;
     }
+    .brand-info__logo {
+      display: block;
+      width: 260px;
+      max-width: 100%;
+      margin: 0 0 28px 0;
+    }
     .brand-info__text {
       margin:  0 0 22px 0;
       font-size: 48px;
       font-weight: 400;
-      text-transform : uppercase;
     }
     .brand-info__intro {
       margin: 10px 0;
