a8e81e4307a00ab71e98671384b2ec7d71f5454a
unknown <yzhan718@its.jnj.com>
2026-05-29 14:53:29 +0800

    chore: configure public frontend access

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -6,9 +6,14 @@
 
   // API base URL
   var host = window.location.hostname
-  window.SITE_CONFIG['baseUrl'] = (host === 'localhost' || host === '127.0.0.1')
-    ? 'http://localhost:8080/renren-fast'
-    : 'http://192.168.0.36:8080/renren-fast'
+  var protocol = window.location.protocol || 'http:'
+  if (host === 'localhost' || host === '127.0.0.1') {
+    window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/renren-fast'
+  } else if (host === '218.202.240.118') {
+    window.SITE_CONFIG['baseUrl'] = 'http://218.202.240.118:8888/renren-fast'
+  } else {
+    window.SITE_CONFIG['baseUrl'] = protocol + '//' + host + ':8080/renren-fast'
+  }
 
   // CDN = domain + version
   window.SITE_CONFIG['domain'] = './'
--- static/config/index-uat.js
+++ static/config/index-uat.js
@@ -5,7 +5,15 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/renren-fast';
+  var host = window.location.hostname;
+  var protocol = window.location.protocol || 'http:';
+  if (host === 'localhost' || host === '127.0.0.1') {
+    window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/renren-fast';
+  } else if (host === '218.202.240.118') {
+    window.SITE_CONFIG['baseUrl'] = 'http://218.202.240.118:8888/renren-fast';
+  } else {
+    window.SITE_CONFIG['baseUrl'] = protocol + '//' + host + ':8080/renren-fast';
+  }
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
